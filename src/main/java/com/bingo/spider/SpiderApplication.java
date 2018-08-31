ae0168188f59c7a250a72dc50332ac1d94858220
yanglbme <szuyanglb@outlook.com>
2018-08-31 22:42:28 +0800

    Initialize spider project

---
+++ src/main/java/com/bingo/spider/SpiderApplication.java
@@ -0,0 +1,17 @@
+package com.bingo.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bingo
+ * @since 2018/8/31
+ */
+
+@SpringBootApplication
+public class SpiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiderApplication.class, args);
+    }
+}
